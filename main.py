3bdc5e84da449c94990f7d5908fac6080c0ad676
Marach8 <nnannamarach4@gmail.com>
2023-09-10 08:07:16 +0000

    Built a year's Calculator

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if b == 'y':
   a = (60*60*24*366)
   print(f'\033[33mThere are {a} seconds in one leap year!\033[0m')
 else:
-  a = 60*60*24*365
+  a = (60*60*24*365)
   print(f'\033[33mThere are {a} seconds in one year!\033[0m')
